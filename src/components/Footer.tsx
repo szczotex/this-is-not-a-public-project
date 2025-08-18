7b2ff6d3ea84e38155b6f0307d2bac7cdddccc91
szczotex <szczotex11@gmail.com>
2025-08-18 20:35:47 +0200

    twardy enter stopka

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="border-t">
       <div className="container py-6 text-sm text-muted-foreground">
-        <p>© 2025 Renewpacks sp. z o.o. </br>26C, 36-002 Wólka Podleśna</p>
+        <p>© 2025 Renewpacks sp. z o.o. <br/>26C, 36-002 Wólka Podleśna</p>
       </div>
     </footer>
   );
