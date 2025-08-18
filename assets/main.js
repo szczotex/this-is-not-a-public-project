458b800a0ff6b232146970018d68431307897615
szczotex <szczotex@users.noreply.github.com>
2025-08-18 17:35:55 +0000

    deploy: 6530aa0a67059b93a949a694de8d23e4e5cd9a6e

--- assets/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-ReactDOM.createRoot(document.getElementById('react-root')).render(
-  React.createElement('div', null,
-    React.createElement('h1', null, 'modu.energy'),
-    React.createElement('p', null, 'Oszczędzamy energię dla Twojej firmy')
-  )
-);
