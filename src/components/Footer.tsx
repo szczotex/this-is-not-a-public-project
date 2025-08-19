9a0c83f2af217c4aa9284c8982097c74527adbb5
szczotex <szczotex11@gmail.com>
2025-08-19 11:28:50 +0200

    zmiana kontenera footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="border-t">
-      <div className="container py-6 text-sm text-muted-foreground">
+      <div className="w-full px-4 max-w-7xl mx-auto py-6 text-sm text-muted-foreground">
         <p>© 2025 Renewpacks sp. z o.o. <br/>26C, 36-002 Wólka Podleśna</p>
       </div>
     </footer>
